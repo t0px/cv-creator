422d46bc6061f3e185575c6290547674be25076f
Meir <meirtubiana@gmail.com>
2023-07-19 05:51:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ function App() {
     // FIXME: fix edit checkmark and pencil change
     // on all education blocks
     //TODO: create professional blocks
+    //TODO: FOOTER
 
   return (
     <PaperProvider>
       <div className="min-h-screen flex justify-center w-full ">
+        <div className="absolute bottom-4">Made with ❤️ by Meir July 2023</div>
         <div className="w-8/12 h-screen relative py-14 flex justify-between gap-10">
-          <div className='flex-col flex gap-4 flex-1'>
+          <div className="flex-col flex gap-4 flex-1">
             <Menu />
             <BottomBar />
           </div>
